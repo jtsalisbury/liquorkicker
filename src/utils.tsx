d579711a31c02ad75a3659b79840ebe1192edaf7
JT Salisbury <crazyscouter1@gmail.com>
2022-07-26 14:50:51 -0400

    remove tech debt

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,14 +2,12 @@ import { Drink } from "./models/Drink";
 
 export const getDrinks = (): Promise<Drink[]> => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            fetch('http://localhost:8080/drinks').then(async response => {
-                if (response.ok) {
+        fetch('http://localhost:8080/drinks').then(async response => {
+            if (response.ok) {
 
-                    const data = await response.json();
-                    resolve(data);
-                }
-            })
-        }, 5000)
+                const data = await response.json();
+                resolve(data);
+            }
+        })
     });
 }
